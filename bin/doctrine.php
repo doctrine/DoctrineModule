ba5f835e8b4c628e4f62ff39b03056eaa15fe7ce
Kyle Spraggs <kyle.spraggs@avimark.net>
2011-10-24 16:09:09 -0500

    Simplified doctrine.php.

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -22,17 +22,10 @@ $moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig->module_paths);
 $moduleLoader->register();
 
 $moduleManager = new Zend\Module\Manager(
-    $appConfig->modules,
+    array('SpiffyDoctrine'),
     new Zend\Module\ManagerOptions($appConfig->module_manager_options)
 );
 
-// Get the merged config object
-$config = $moduleManager->getMergedConfig();
-
-// Create application, bootstrap, and run
-$bootstrap = new $config->bootstrap_class($config, $moduleManager);
-
-// Here's the fun-stuff, Doctrine CLI.
-$helperSet = ($helperSet) ?: new \Symfony\Component\Console\Helper\HelperSet();
-
-\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
+\Doctrine\ORM\Tools\Console\ConsoleRunner::run(
+    new \Symfony\Component\Console\Helper\HelperSet()
+);
