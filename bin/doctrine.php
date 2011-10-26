c21ed488da9dfcf257b77e28960167e757fa7b1a
Kyle Spraggs <kyle.spraggs@avimark.net>
2011-10-26 13:37:25 -0500

    Updated Doctrine submodule to be just doctrine-orm. Updated classmap. Updated doctrine/bin.

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -9,21 +9,22 @@ defined('APPLICATION_ENV')
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(__DIR__ . '/../../../lib'), // just for git submodule
+    realpath(__DIR__ . '/../../../library'), // just for git submodule
+    realpath(__DIR__ . '/../../../library/ZendFramework/library'), // just for git submodule
     get_include_path(),
 )));
 
 require_once 'Zend/Loader/AutoloaderFactory.php';
 Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
 
-$appConfig = include __DIR__ . '/../../../etc/application.config.php';
+$appConfig = new Zend\Config\Config(include __DIR__ . '/../../../configs/application.config.php');
 
-$moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig->module_paths);
+$moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig['module_paths']);
 $moduleLoader->register();
 
 $moduleManager = new Zend\Module\Manager(
-    array('SpiffyDoctrine'),
-    new Zend\Module\ManagerOptions($appConfig->module_manager_options)
+    array('SpiffyDoctrine', 'Application'),
+    new Zend\Module\ManagerOptions($appConfig['module_manager_options'])
 );
 
 \Doctrine\ORM\Tools\Console\ConsoleRunner::run(
