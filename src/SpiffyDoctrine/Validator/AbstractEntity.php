6e7b40a7fc2d86ff6dbc992b7aac5f8ea13a2617
Kyle Spraggs <kyle.spraggs@avimark.net>
2011-10-29 11:41:42 -0500

    Added DocBlocks.

--- src/SpiffyDoctrine/Validator/AbstractEntity.php
+++ src/SpiffyDoctrine/Validator/AbstractEntity.php
@@ -22,17 +22,46 @@ abstract class AbstractEntity extends AbstractValidator
         self::ERROR_NO_RECORD_FOUND => "No record matching '%value%' was found",
         self::ERROR_RECORD_FOUND    => "A record matching '%value%' was found",
     );
-    
+ 
+    /**
+     * Doctrine EntityManager
+     * 
+     * @var EntityManager
+     */
     protected $_em;
     
+    /**
+     * Name of the entity to use for validation.
+     * 
+     * @var string
+     */
     protected $_entity;
     
+    /**
+     * Name of the field to use for validation.
+     * 
+     * @var string
+     */
     protected $_field;
-    
-    protected $_query;
-    
+
+    /**
+     * QueryBuilder closure.
+     * 
+     * @var Closure
+     */
     protected $_queryBuilder;
 
+    /**
+     * Required options are:
+     *  - em        EntityManager instance to use.
+     *  - entity    Entity to use for validation.
+     *  - field     Field to check.
+     * 
+     * Optional options are:
+     *  - query_builder     Custom query_builder Closure to use for the select. A single
+     *                      EntityRepository argument is passed at call time. Example:
+     *                      'query_builder' => function($er) { return $er->createQueryBuilder('q'); } 
+     */
     public function __construct(array $options)
     {
         if (!array_key_exists('em', $options)) {
@@ -49,9 +78,7 @@ abstract class AbstractEntity extends AbstractValidator
         }
         $this->_entity = $options['entity'];
         
-        if (array_key_exists('query', $options)) {
-            $this->_query = $options['query'];
-        } else if (array_key_exists('query_builder', $options)) {
+        if (array_key_exists('query_builder', $options)) {
             if (!$options['query_builder'] instanceof Closure) {
                 throw new Exception\InvalidArgumentException('query_builder must be a Closure');
             }
@@ -64,18 +91,22 @@ abstract class AbstractEntity extends AbstractValidator
         $this->_field = $options['field'];
     }
 
+    /**
+     * Generates a query based on the constructor options. Attempts to enforce
+     * a minimum select for the query by checking the DQL parts for an alias
+     * and then using the entity metadata to select only the identifiers.
+     * 
+     * @param mixed $value  Contains the value to check.
+     * @return Doctrine\ORM\Query 
+     */
     protected function _getQuery($value)
     {
-        if ($this->_query) {
-            return $this->_query;
-        }
-        
         $em = $this->_em;
         $qb = $this->_queryBuilder;
         $field = $this->_field;
         
-        // generate default query builder closure if one wasn't specified
-        if (!$qb instanceof Closure) {
+        // set default query builder closure if one wasn't specified
+        if (!$qb) {
             $qb = function(EntityRepository $er) use ($field) {
                 return $er->createQueryBuilder('q');
             };
@@ -88,11 +119,7 @@ abstract class AbstractEntity extends AbstractValidator
         $mdata = $em->getClassMetadata($this->_entity);
         $alias = current($qb->getDqlPart('from'))->getAlias();
         
-        $select = "partial {$alias}.{";
-        $select.= implode(',', $mdata->identifier);
-        $select.= '}';
-        
-        $qb->select($select);
+        $qb->select("partial {$alias}.{" . implode(',', $mdata->identifier) . '}');
         
         // add field to query
         $qb->andWhere($qb->expr()->eq('q.' . $field, $qb->expr()->literal($value)))
