6d43c8494669b2b56fe25778dade690fb155619f
Tom Anderson <tom.h.anderson@gmail.com>
2012-05-18 12:16:01 -0700

    Change for http://zend-framework-community.634137.n4.nabble.com/ServiceManager-MVC-integration-merged-with-master-BC-breaking-changes-td4643952.html

--- Module.php
+++ Module.php
@@ -20,8 +20,7 @@
 namespace DoctrineModule;
 
 use Zend\EventManager\Event;
-use Zend\Module\Consumer\AutoloaderProvider;
-use Zend\Module\Manager;
+use Zend\ModuleManager\ModuleManager;
 use Zend\Loader\StandardAutoloader;
 
 /**
@@ -34,7 +33,7 @@ use Zend\Loader\StandardAutoloader;
  * @author  Kyle Spraggs <theman@spiffyjr.me>
  * @author  Marco Pivetta <ocramius@gmail.com>
  */
-class Module implements AutoloaderProvider
+class Module
 {
     /**
      * Retrieves configuration that can be consumed by Zend\Loader\AutoloaderFactory
