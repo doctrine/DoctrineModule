6f8d1d3d0f08fd83baec5783b188b1b40ebb4c3c
Marco Pivetta <ocramius@gmail.com>
2012-05-20 21:29:21 +0200

    Fixing CS, removing unused code

--- bin/doctrine-module.php
+++ bin/doctrine-module.php
@@ -17,27 +17,30 @@
  * <http://www.doctrine-project.org>.
  */
 
+use Zend\Loader\AutoloaderFactory;
+use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\Service\ServiceManagerConfiguration;
+
 ini_set('display_errors', true);
 chdir(__DIR__);
 
 $previousDir = '.';
+
 while (!file_exists('config/application.config.php')) {
     $dir = dirname(getcwd());
+
     if ($previousDir === $dir) {
         throw new RuntimeException(
             'Unable to locate "config/application.config.php":'
                 . ' is DoctrineModule in a subdir of your application skeleton?'
         );
     }
+
     $previousDir = $dir;
     chdir($dir);
 }
 
 require_once (getenv('ZF2_PATH') ?: 'vendor/ZendFramework/library') . '/Zend/Loader/AutoloaderFactory.php';
-
-use Zend\Loader\AutoloaderFactory,
-Zend\ServiceManager\ServiceManager,
-Zend\Mvc\Service\ServiceManagerConfiguration;
 
 // setup autoloader
 AutoloaderFactory::factory();
@@ -49,9 +52,7 @@ $configuration = include 'config/application.config.php';
 $serviceManager = new ServiceManager(new ServiceManagerConfiguration($configuration['service_manager']));
 $serviceManager->setService('ApplicationConfiguration', $configuration);
 $serviceManager->get('ModuleManager')->loadModules();
-$application = $serviceManager->get('Application');
 
 $serviceManager
-    ->get('Di')
     ->get('doctrine_cli')
     ->run();
