f9abeaad2d3f91b959c359f0867913a2cf5cdf94
Marco Pivetta <ocramius@gmail.com>
2012-06-02 10:38:57 +0200

    Fixing path of config (forgot to update before)

--- src/DoctrineModule/Module.php
+++ src/DoctrineModule/Module.php
@@ -37,6 +37,6 @@ class Module
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 }
