b4dc0c77bd70ee5bc8754f9595014d4e7cb412a3
Marco Pivetta <ocramius@gmail.com>
2012-07-04 22:24:34 +0200

    Fixing #41 and testing new functionality

--- src/DoctrineModule/Service/EventManagerFactory.php
+++ src/DoctrineModule/Service/EventManagerFactory.php
@@ -2,28 +2,52 @@
 
 namespace DoctrineModule\Service;
 
-use RuntimeException;
+use InvalidArgumentException;
 use Doctrine\Common\EventManager;
+use Doctrine\Common\EventSubscriber;
 use DoctrineModule\Service\AbstractFactory;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
+/**
+ * Factory responsible for creating EventManager instances
+ */
 class EventManagerFactory extends AbstractFactory
 {
+    /**
+     * {@inheritDoc}
+     */
     public function createService(ServiceLocatorInterface $sl)
     {
         /** @var $options \DoctrineModule\Options\EventManager */
-        $options = $this->getOptions($sl, 'eventmanager');
-        $evm     = new EventManager;
+        $options      = $this->getOptions($sl, 'eventmanager');
+        $eventManager = new EventManager();
 
-        foreach($options->getSubscribers() as $subscriber) {
-            if (is_subclass_of($subscriber, 'Doctrine\Common\EventSubscriber')) {
-                $evm->addEventSubscriber(new $subscriber);
-            } else {
-                $evm->addEventSubscriber($sl->get($subscriber));
+        foreach($options->getSubscribers() as $subscriberName) {
+            $subscriber = $subscriberName;
+
+            if (is_string($subscriber)) {
+                if ($sl->has($subscriber)) {
+                    $subscriber = $sl->get($subscriber);
+                } else if (class_exists($subscriber)) {
+                    $subscriber = new $subscriber();
+                }
             }
+
+            if ($subscriber instanceof EventSubscriber) {
+                $eventManager->addEventSubscriber($subscriber);
+                continue;
+            }
+
+            throw new InvalidArgumentException(sprintf(
+                'Invalid event subscriber "%s" given, must be a service name, '
+                . 'class name or an instance implementing Doctrine\Common\EventSubscriber',
+                is_object($subscriberName)
+                    ? get_class($subscriberName)
+                    : (is_string($subscriberName) ? $subscriberName : gettype($subscriber))
+            ));
         }
 
-        return $evm;
+        return $eventManager;
     }
 
     /**
