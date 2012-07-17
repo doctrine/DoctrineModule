58861a039fd6eedecb0150e781ba3fd1373391f2
Marco Pivetta <ocramius@gmail.com>
2012-07-17 20:24:29 +0200

    Adding compatibility file to allow loading of the module even without composer in place

---
+++ Module.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/DoctrineModule' in order to respect PSR-0
+ */
+require_once __DIR__ . '/src/DoctrineModule/Module.php';
