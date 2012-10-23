15b095a8b00f4c61204b7a3c1ffc24278c8344f4
Michaël Gallego <mic.gallego@gmail.com>
2012-10-23 12:45:02 +0200

    Provides ObjectRepository for forms

--- src/DoctrineModule/Form/ObjectRepositoryProvider.php
+++ src/DoctrineModule/Form/ObjectRepositoryProvider.php
@@ -17,56 +17,39 @@
  * <http://www.doctrine-project.org>.
  */
 
-namespace DoctrineModule\Form\Element;
+namespace DoctrineModule\Form;
 
-use DoctrineModule\Form\Element\Proxy;
-use Zend\Form\Element\MultiCheckbox;
-use Zend\Form\Form;
+use Doctrine\Common\Persistence\ObjectRepository;
 
-class ObjectMultiCheckbox extends MultiCheckbox
+/**
+ * Trait to provide object repository to a form (only works from PHP 5.4)
+ */
+trait ProvidesObjectRepository
 {
     /**
-     * @var Proxy
+     * @var ObjectRepository
      */
-    protected $proxy;
+    protected $objectRepository;
 
     /**
-     * @return Proxy
+     * Set the object repository
+     *
+     * @param  ObjectRepository $objectRepository
+     * @return ProvidesObjectRepository
      */
-    public function getProxy()
+    public function setObjectRepository(ObjectRepository $objectRepository)
     {
-        if (null === $this->proxy) {
-            $this->proxy = new Proxy();
-        }
-        return $this->proxy;
+        $this->objectRepository = $objectRepository;
+        return $this;
     }
 
     /**
-     * @param  array|\Traversable $options
-     * @return ObjectSelect
+     * Get the object repository
+     *
+     * @return ObjectRepository
      */
-    public function setOptions($options)
+    public function getObjectRepository()
     {
-        $this->getProxy()->setOptions($options);
-        return parent::setOptions($options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function setValue($value)
-    {
-        return parent::setValue($this->getProxy()->getValue($value));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getValueOptions()
-    {
-        if (empty($this->valueOptions)) {
-            $this->setValueOptions($this->getProxy()->getValueOptions());
-        }
-        return $this->valueOptions;
+        return $this->getObjectRepository();
     }
 }
---
+++ src/DoctrineModule/Form/ObjectRepositoryProviderInterface.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace DoctrineModule\Form;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+
+interface ObjectRepositoryProviderInterface
+{
+    /**
+     * Set the object repository
+     *
+     * @param ObjectRepository $objectRepository
+     */
+    public function setObjectRepository(ObjectRepository $objectRepository);
+
+    /**
+     * Get the object repository
+     *
+     * @return ObjectRepository
+     */
+    public function getObjectRepository();
+}
