c3c446b247871a006619a30f186114a6f015b1a3
David Windell <david@outeredgeuk.com>
2013-01-16 05:20:20 -0800

    Create OneToOneEntityNotNullable.php

---
+++ tests/DoctrineModuleTest/Stdlib/Hydrator/Asset/OneToOneEntityNotNullable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DoctrineModuleTest\Stdlib\Hydrator\Asset;
+
+
+class OneToOneEntityNotNullable
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var SimpleEntity
+     */
+    protected $toOne;
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setToOne(SimpleEntity $entity, $modifyValue = true)
+    {
+        // Modify the value to illustrate the difference between by value and by reference
+        if ($modifyValue) {
+            $entity->setField('Modified from setToOne setter', false);
+        }
+
+        $this->toOne = $entity;
+    }
+
+    public function getToOne($modifyValue = true)
+    {
+        // Make some modifications to the association so that we can demonstrate difference between
+        // by value and by reference
+        if ($modifyValue) {
+            $this->toOne->setField('Modified from getToOne getter', false);
+        }
+
+        return $this->toOne;
+    }
+}
