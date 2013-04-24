1e714e8f3bb6e0e4a4b2f02f41e019f54c8a057a
Marco Pivetta <ocramius@gmail.com>
2013-04-24 22:21:18 +0200

    Setting default cache namespace to 'DoctrineModule'

--- config/module.config.php
+++ config/module.config.php
@@ -21,35 +21,44 @@ return array(
     'doctrine' => array(
         'cache' => array(
             'apc' => array(
-                'class' => 'Doctrine\Common\Cache\ApcCache',
+                'class'     => 'Doctrine\Common\Cache\ApcCache',
+                'namespace' => 'DoctrineModule',
             ),
             'array' => array(
                 'class' => 'Doctrine\Common\Cache\ArrayCache',
+                'namespace' => 'DoctrineModule',
             ),
             'filesystem' => array(
-                'class' => 'Doctrine\Common\Cache\FilesystemCache',
+                'class'     => 'Doctrine\Common\Cache\FilesystemCache',
                 'directory' => 'data/DoctrineModule/cache',
+                'namespace' => 'DoctrineModule',
             ),
             'memcache' => array(
-                'class'    => 'Doctrine\Common\Cache\MemcacheCache',
-                'instance' => 'my_memcache_alias',
+                'class'     => 'Doctrine\Common\Cache\MemcacheCache',
+                'instance'  => 'my_memcache_alias',
+                'namespace' => 'DoctrineModule',
             ),
             'memcached' => array(
-                'class'    => 'Doctrine\Common\Cache\MemcachedCache',
-                'instance' => 'my_memcached_alias',
+                'class'     => 'Doctrine\Common\Cache\MemcachedCache',
+                'instance'  => 'my_memcached_alias',
+                'namespace' => 'DoctrineModule',
             ),
             'redis' => array(
-                'class'    => 'Doctrine\Common\Cache\RedisCache',
-                'instance' => 'my_redis_alias',
+                'class'     => 'Doctrine\Common\Cache\RedisCache',
+                'instance'  => 'my_redis_alias',
+                'namespace' => 'DoctrineModule',
             ),
             'wincache' => array(
-                'class' => 'Doctrine\Common\Cache\WinCacheCache',
+                'class'     => 'Doctrine\Common\Cache\WinCacheCache',
+                'namespace' => 'DoctrineModule',
             ),
             'xcache' => array(
-                'class' => 'Doctrine\Common\Cache\XcacheCache',
+                'class'     => 'Doctrine\Common\Cache\XcacheCache',
+                'namespace' => 'DoctrineModule',
             ),
             'zenddata' => array(
-                'class' => 'Doctrine\Common\Cache\ZendDataCache',
+                'class'     => 'Doctrine\Common\Cache\ZendDataCache',
+                'namespace' => 'DoctrineModule',
             ),
         ),
     ),
