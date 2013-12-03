e2dcb82723ecaa9a9fd8995d662c7ab4b637b1a8
Martin Keckeis <martin.keckeis1@gmail.com>
2013-12-03 19:20:26 +0100

    Update PropertyOutputTest.php

--- tests/DoctrineModuleTest/Component/Console/Output/PropertyOutputTest.php
+++ tests/DoctrineModuleTest/Component/Console/Output/PropertyOutputTest.php
@@ -19,6 +19,7 @@
 namespace DoctrineModuleTest\Component\Console\Output;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use DoctrineModule\Component\Console\Output\PropertyOutput;
 
 /**
  * Tests for {@see \DoctrineModule\Component\Console\Output\PropertyOutput}
@@ -39,5 +40,21 @@ class PropertyOutputTest extends TestCase
         $output = new \DoctrineModule\Component\Console\Output\PropertyOutput();
         $output->write($message);
         $this->assertEquals($message, $output->getMessage());
+    }
+    
+    /**
+     * @covers \DoctrineModule\Component\Console\Output\PropertyOutput
+     */
+    public function testWriteConcat()
+    {
+        $message = 'message';
+        $message2 = 'message2';
+        
+        $output = new PropertyOutput();
+        $output->write($message, PHP_EOL);
+        $output->write($message2, PHP_EOL);
+        
+        $expected = $message . PHP_EOL . $message2 . PHP_EOL;
+        $this->assertEquals($expected, $output->getMessage());
     }
 }
