9fc7b1df14468cfeb6463b8accf3b3f228806240
Martin Keckeis <martin.keckeis1@gmail.com>
2013-12-03 18:01:43 +0100

    Test with console mocking

--- tests/DoctrineModuleTest/ModuleTest.php
+++ tests/DoctrineModuleTest/ModuleTest.php
@@ -109,12 +109,36 @@ class ModuleTest extends PHPUnit_Framework_TestCase
      */
     public function testGetConsoleUsage()
     {
+        $cliMock = $this->getMock('Symfony\Component\Console\Application', array('setDispatcher', 'run'), array(), '', false, false);
+        $cliMock->expects($this->any())
+            ->method('run')
+            ->will($this->returnCallback(function ($input, $output)
+        {
+            if($input == 'list'){
+                $output->write('start', true);
+                $output->write('Line2', true);
+                $output->write('Line3');
+                $output->write('Line4');
+                $output->write('end');
+            }
+        }));
+
+        /* @var $sm \Zend\ServiceManager\ServiceManager */
+        $sm = $this->eventMock->getTarget()->getServiceManager();
+        $cliOriginal = $sm->get('doctrine.cli');
+        
+        $sm->setAllowOverride(true);
+        $sm->setService('doctrine.cli', $cliMock);
+        
         $module = new Module();
         $module->onBootstrap($this->eventMock);
         
         $console = $this->getMock('Zend\Console\Adapter\AbstractAdapter');
         $actual = $module->getConsoleUsage($console);
+
+        $this->assertStringMatchesFormat("start%aend", $actual);
         
-        $this->assertStringMatchesFormat("DoctrineModule Command Line Interface%aLists commands", $actual);
+        $sm->setService('doctrine.cli', $cliOriginal);
+        $sm->setAllowOverride(false);
     }
 }
