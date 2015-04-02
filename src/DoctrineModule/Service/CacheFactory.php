219842b4acd83fe26eb87407318a08f00d8b7b29
Bilge <bilge@scriptfusion.com>
2015-04-02 16:38:00 +0000

    Added PredisCache support.

--- src/DoctrineModule/Service/CacheFactory.php
+++ src/DoctrineModule/Service/CacheFactory.php
@@ -24,7 +24,6 @@ use RuntimeException;
 use Doctrine\Common\Cache\MemcacheCache;
 use Doctrine\Common\Cache\MemcachedCache;
 use Doctrine\Common\Cache\RedisCache;
-use DoctrineModule\Service\AbstractFactory;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
@@ -53,16 +52,23 @@ class CacheFactory extends AbstractFactory
             throw new RuntimeException('Cache must have a class name to instantiate');
         }
 
-        if ('filesystem' === $this->name) {
-            $cache = new $class($options->getDirectory());
-        } else {
-            $cache = new $class;
-        }
-
         $instance = $options->getInstance();
 
         if (is_string($instance) && $serviceLocator->has($instance)) {
             $instance = $serviceLocator->get($instance);
+        }
+
+        switch ($this->name) {
+            case 'filesystem':
+                $cache = new $class($options->getDirectory());
+                break;
+
+            case 'predis':
+                $cache = new $class($instance);
+                break;
+
+            default:
+                $cache = new $class;
         }
 
         if ($cache instanceof MemcacheCache) {
@@ -75,8 +81,6 @@ class CacheFactory extends AbstractFactory
             /* @var $cache RedisCache */
             $cache->setRedis($instance);
         }
-
-        ;
 
         if ($cache instanceof CacheProvider && ($namespace = $options->getNamespace())) {
             $cache->setNamespace($namespace);
