62730ff1a0a9dcbd543e083ea1d9869c74a723a3
Satans Krümelmonster <lukaskullmann@web.de>
2016-02-29 19:47:34 +0100

    Added possibility to override default hydration-strategy. This fixes Issue #538.

---
+++ tests/DoctrineModuleTest/Stdlib/Hydrator/Asset/DifferentAllowRemoveByValue.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DoctrineModuleTest\Stdlib\Hydrator\Asset;
+
+use DoctrineModule\Stdlib\Hydrator\Strategy\AllowRemoveByValue;
+
+class DifferentAllowRemoveByValue extends AllowRemoveByValue {
+
+}
