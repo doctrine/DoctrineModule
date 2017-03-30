e8fa2f2c0e893c0e1cb4aca56a672b63c8936751
James Titcumb <james@asgrim.com>
2017-03-30 12:29:42 +0100

    Added more tests for keys and serialziation for ConfigProvider

--- tests/DoctrineModuleTest/ConfigProviderTest.php
+++ tests/DoctrineModuleTest/ConfigProviderTest.php
@@ -31,11 +31,22 @@ use PHPUnit_Framework_TestCase;
  */
 class ConfigProviderTest extends PHPUnit_Framework_TestCase
 {
-    public function testInvokeHasDependencyKeyAndNotServiceManager()
+    public function testInvokeHasCorrectKeys()
     {
         $config = (new ConfigProvider())->__invoke();
 
+        self::assertInternalType('array', $config);
+
+        self::assertArrayHasKey('doctrine', $config, 'Expected config to have "doctrine" array key');
+        self::assertArrayHasKey('doctrine_factories', $config, 'Expected config to have "doctrine_factories" array key');
         self::assertArrayHasKey('dependencies', $config, 'Expected config to have "dependencies" array key');
+        self::assertArrayHasKey('controllers', $config, 'Expected config to have "controllers" array key');
+        self::assertArrayHasKey('route_manager', $config, 'Expected config to have "route_manager" array key');
+        self::assertArrayHasKey('console', $config, 'Expected config to have "console" array key');
+
+        // Config Provider should not have service_manager key; should only exist in ZF Module
         self::assertArrayNotHasKey('service_manager', $config, 'Config should not have "service_manager" array key');
+
+        self::assertSame($config, unserialize(serialize($config)));
     }
 }
