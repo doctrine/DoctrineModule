e420353f46c995f1301ff50d31d6bb6e5ebbce90
Tom H Anderson <tom.h.anderson@gmail.com>
2020-01-22 09:08:25 -0700

    Removed hydrator

--- tests/DoctrineModuleTest/Stdlib/Hydrator/Asset/DifferentAllowRemoveByReference.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DoctrineModuleTest\Stdlib\Hydrator\Asset;
-
-use DoctrineModule\Stdlib\Hydrator\Strategy\AllowRemoveByReference;
-
-class DifferentAllowRemoveByReference extends AllowRemoveByReference {
-
-}
--- tests/DoctrineModuleTest/Stdlib/Hydrator/Asset/DifferentAllowRemoveByValue.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DoctrineModuleTest\Stdlib\Hydrator\Asset;
-
-use DoctrineModule\Stdlib\Hydrator\Strategy\AllowRemoveByValue;
-
-class DifferentAllowRemoveByValue extends AllowRemoveByValue {
-
-}
