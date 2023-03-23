45f3d0123f2f1d3858df0f223b9c760cb5c80335
Demian Katz <demian.katz@villanova.edu>
2023-03-23 14:39:41 -0400

    Fix styles.

--- tests/Service/CacheFactoryTest.php
+++ tests/Service/CacheFactoryTest.php
@@ -11,8 +11,6 @@ use DoctrineModule\Cache\LaminasStorageCache;
 use DoctrineModule\Service\CacheFactory;
 use Laminas\Cache\ConfigProvider;
 use Laminas\Cache\Storage\Adapter\BlackHole;
-use Laminas\Cache\Storage\AdapterPluginManager;
-use Laminas\ServiceManager\Factory\InvokableFactory;
 use Laminas\ServiceManager\ServiceManager;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
@@ -77,7 +75,7 @@ class CacheFactoryTest extends BaseTestCase
             $serviceManager->configure((new BlackHole\ConfigProvider())->getServiceDependencies());
             $serviceManager->setService('config', $config);
         } else {
-            $this->markTestSkipped("Test requires blackhole adapter 2");
+            $this->markTestSkipped('Test requires blackhole adapter 2');
         }
 
         $cache = $factory->__invoke($serviceManager, LaminasStorageCache::class);
